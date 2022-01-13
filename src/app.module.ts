06b2b3bfde6c10481d3f6849ec281ebee355e9ed
Exauce mwililikwa <exaucemwililikwa@gmail.com>
2022-01-13 16:03:33 +0200

    migration

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import * as ormOptions from './config/orm'
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(ormOptions),RepoModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
